acd634d197839afd4f1854f11d0a109f5120ca2f
Indra Gunawan <ingunawandra@gmail.com>
2022-10-19 09:36:41 +0000

    feat: category crud endpoint

--- models/category.py
+++ models/category.py
@@ -1,7 +1,12 @@
 """
 category model
 """
+from __future__ import annotations
 
+from http import HTTPStatus
+from typing import List, Tuple
+
+from sqlalchemy.exc import NoResultFound
 from sqlalchemy.sql import func
 
 from app import db
@@ -13,9 +18,74 @@ class Category(db.Model):
     name = db.Column(db.String(255), nullable=False)
     image_url = db.Column(db.String(255))
     sort = db.Column(db.Integer)
-    products = db.relationship("Product", back_populates="category", lazy="dynamic")
+    products = db.relationship(
+        "Product", back_populates="category", lazy="dynamic"
+    )  # noqa
     created_at = db.Column(
         db.DateTime(timezone=True), nullable=False, server_default=func.now()
     )
     updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
     # TODO: implement soft delete
+
+    @classmethod
+    def create(cls, payload) -> Tuple[int, Category]:
+        new_category = Category(**payload)
+        try:
+            db.session.add(new_category)
+            db.session.commit()
+        except Exception as e:
+            return HTTPStatus.INTERNAL_SERVER_ERROR, str(e)
+        finally:
+            db.session.close()
+
+        return HTTPStatus.OK, payload
+
+    @classmethod
+    def update(cls, payload, category_id) -> Tuple[int, Category]:
+        category = None
+        try:
+            category = (
+                db.session.query(Category).filter_by(id=category_id).one()
+            )  # noqa
+        except NoResultFound as e:
+            return HTTPStatus.NOT_FOUND, str(e)
+
+        try:
+            category.name = payload["name"] or category.name
+            category.image_url = payload["image_url"] or category.image_url
+            category.sort = payload["sort"] or category.sort
+            db.session.commit()
+        except Exception as e:
+            return HTTPStatus.INTERNAL_SERVER_ERROR, str(e)
+        finally:
+            db.session.close()
+
+        return HTTPStatus.OK, category
+
+    @classmethod
+    def get_by_id(cls, category_id) -> Tuple[int, Category]:
+        category = None
+        try:
+            category = (
+                db.session.query(Category).filter_by(id=category_id).one()
+            )  # noqa
+        except NoResultFound as e:
+            return HTTPStatus.NOT_FOUND, str(e)
+        except Exception as e:
+            return HTTPStatus.INTERNAL_SERVER_ERROR, str(e)
+        finally:
+            db.session.close()
+
+        return HTTPStatus.OK, category
+
+    @classmethod
+    def get_all(cls) -> Tuple[int, List[Category]]:
+        categories = None
+        try:
+            categories = db.session.query(Category).all()
+        except Exception as e:
+            return HTTPStatus.INTERNAL_SERVER_ERROR, str(e)
+        finally:
+            db.session.close()
+
+        return HTTPStatus.OK, categories
--- resources/category.py
+++ resources/category.py
@@ -1,35 +1,50 @@
-# from flask import request
-from flask.views import MethodView
+from http import HTTPStatus
+
 from flask_smorest import Blueprint, abort
 
-from databases.db import db
 from models import Category
-from schema import CategorySchema, PlainCategorySchema
+from schema import PlainCategorySchema
 
 category_blueprint = Blueprint(
     "categories", __name__, description="Operation on Category"
 )
 
 
-@category_blueprint.route("/categories")
-class CategoryController(MethodView):
-    def get(self):
-        pass
-
-    @category_blueprint.arguments(PlainCategorySchema)
-    @category_blueprint.response(201, PlainCategorySchema)
-    def post(self, payload):
-        category = Category(**payload)
-
-        try:
-            db.session.add(category)
-            db.session.commit()
-        except Exception as e:
-            abort(500, message=str(e))
+@category_blueprint.route("/categories", methods=["GET"])
+@category_blueprint.response(200, PlainCategorySchema(many=True))
+def get_categories():
+    status, result = Category().get_all()
+    if not (status == HTTPStatus.OK):
+        abort(status, result)
+    return result
 
 
-@category_blueprint.route("/categories/<int:category_id>")
-@category_blueprint.response(200, CategorySchema)
-class GetCategory(MethodView):
-    def get(self, category_id):
-        pass
+@category_blueprint.route("/categories/<int:category_id>", methods=["GET"])
+@category_blueprint.response(200, PlainCategorySchema)
+def get_category_by_id(category_id: int):
+    status, result = Category.get_by_id(category_id)
+    if not (status == HTTPStatus.OK):
+        abort(status, result)
+    return result
+
+
+@category_blueprint.route("/categories", methods=["POST"])
+@category_blueprint.arguments(PlainCategorySchema)
+@category_blueprint.response(201, PlainCategorySchema)
+def create_category(payload):
+    status, result = Category().create(payload)
+    if not (status == HTTPStatus.OK):
+        abort(status, result)
+
+    return result
+
+
+@category_blueprint.route("/categories/<int:category_id>", methods=["PUT"])
+@category_blueprint.arguments(PlainCategorySchema)
+@category_blueprint.response(202, PlainCategorySchema)
+def update_category(payload, category_id: int):
+    status, result = Category().update(payload, category_id)
+    if not (status == HTTPStatus.OK):
+        abort(status, result)
+
+    return result
